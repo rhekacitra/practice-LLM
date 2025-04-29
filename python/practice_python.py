84b7daa8fbb4aec92e9b20436819250ef47d6d5a
rhekacitra <80679977+rhekacitra@users.noreply.github.com>
2025-04-29 08:29:51 -0700

    added EDA and data cleaning

--- python/practice_python.py
+++ python/practice_python.py
@@ -10,7 +10,7 @@ df_clean = df.dropna(subset=['category'])
 # Count articles per category
 category_counts = df_clean['category'].value_counts()
 
-# Plot a bar graph
+# Plot a bar graph to a png
 plt.figure(figsize=(10, 6))
 category_counts.plot(kind='bar')
 plt.title('Number of Articles per Category')
@@ -18,5 +18,5 @@ plt.xlabel('Category')
 plt.ylabel('Number of Articles')
 plt.xticks(rotation=45)
 plt.tight_layout()
-plt.savefig('practice_python_plot.png')
+plt.savefig('python/practice_python_plot.png')
 plt.show()
